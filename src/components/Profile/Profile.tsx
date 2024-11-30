2bac3a6acb79e947f44d5a3c31c750f1ae4efff6
iuliia sinkova <juliasinkova97@gmail.com>
2024-11-30 12:09:00 +0400

    add components (Button, Profile)

---
+++ src/components/Profile/Profile.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Profile = () => {
+  return (
+    <div>
+        <img src="https://upload.wikimedia.org/wikipedia/commons/9/99/Sample_User_Icon.png" alt="user" />
+    </div>
+  )
+}
+
+export default Profile
